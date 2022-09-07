13a6b205fdec48fab0232cf6a29bdbf70142d534
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-09-07 11:38:20 +0300

    set up React

--- Chat_App/app/src/App.js
+++ Chat_App/app/src/App.js
@@ -1,7 +1,33 @@
 import './App.css';
+import { useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:3001');
 
 function App() {
-    return <div className='App'></div>;
+    const [username, setUserame] = useState('');
+    const [room, setRoom] = useState('');
+
+    const joinRoom = () => {
+        if (username !== '' && room !== '') socket.emit('join_room', room);
+    };
+
+    return (
+        <div className='App'>
+            <h3>Join Chat</h3>
+            <input
+                type='text'
+                placeholder='username...'
+                onChange={(e) => setUserame(e.target.value)}
+            />
+            <input
+                type='text'
+                placeholder='room...'
+                onChange={(e) => setRoom(e.target.value)}
+            />
+            <button onClick={joinRoom}>Join</button>
+        </div>
+    );
 }
 
 export default App;
