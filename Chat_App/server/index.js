ee4b4b42c516da3d72d246a7a8fe929f3b08624a
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-09-07 11:38:04 +0300

    set up the server

--- Chat_App/server/index.js
+++ Chat_App/server/index.js
@@ -16,6 +16,11 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket) => {
+    socket.on('join_room', (data) => {
+        socket.join(data);
+        console.log(`User: ${socket.id} join room: ${data}`);
+    });
+
     socket.on('disconnect', () => console.log('User disconnected', socket.id));
 });
 
